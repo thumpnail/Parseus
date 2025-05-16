a5c1f8cbdb9d7686d93514f9646898d0cf4357db
Fenex <31255152+thumpnail@users.noreply.github.com>
2025-05-16 19:42:15 +0200

    deletion

--- Helper.cs
+++
@@ -1,8 +0,0 @@
-﻿using Newtonsoft.Json;
-namespace parser;
-public static class Helper {
-    public static string Dump(this object obj) {
-        return JsonConvert.SerializeObject(obj, Formatting.Indented);
-    }
-}
-
