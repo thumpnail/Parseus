acd7e6e820fbab4fe4e61722beecc0e660eed469
Fenex <31255152+thumpnail@users.noreply.github.com>
2025-05-23 16:22:01 +0200

    Update SbnfParser_Parseus.cs

--- src/Parser/BasicParsers/SBNF_new/SbnfParser_Parseus.cs
+++ src/Parser/BasicParsers/SBNF_new/SbnfParser_Parseus.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Parseus.Parser.Common;
 using Parseus.Parser.Implicit;
 using Parseus.Lexer;
@@ -5,30 +6,82 @@ namespace Parseus.Parser.BasicParsers.SBNF_new;
 
 public class SbnfParser_Parseus : BaseParser {
     // SbnfDocument = { rule } ;
+    private static List<(string key, List<string> value)> typeMap = new();
     public record SbnfDocument {
         public List<SbnfRule> Rules = new();
+        public override string ToString() {
+            StringBuilder sb = new();
+            foreach (var rule in Rules) {
+                sb.Append(rule.ToString());
+            }
+            foreach (var type in typeMap) {
+                sb.Append($"public record {type.key} {{");
+                foreach (var value in type.value) {
+                    sb.Append($"public object {value};");
+                }
+                sb.Append($"}}");
+            }
+            return sb.ToString();
+        }
     }
 
     // rule = identifier ':=' alternation ;
     public record SbnfRule {
         public string Identifier;
         public SbnfAlternation Alternation;
+        public override string ToString() {
+            StringBuilder sb = new();
+            sb.Append($"private static Parser<Generated_{Identifier.ToUpper()}> Generated_{Identifier.ToUpper()}Parser = new((c, self) => {{");
+            typeMap.Add(($"Generated_{Identifier.ToUpper()}", new()));
+            sb.Append(Alternation.ToString());
+            sb.Append($"}});");
+            return sb.ToString();
+        }
     }
 
     // alternation = concatenation { '|' concatenation } ;
     public record SbnfAlternation {
         public List<SbnfConcatenation> Concatenations = new();
+        public override string ToString() {
+            StringBuilder sb = new();
+            if (Concatenations.Count > 1) {
+                sb.Append($"Alt(c,");
+                foreach (var concat in Concatenations) {
+                    sb.Append($"c=> {{");
+                    sb.Append(concat.ToString());
+                    sb.Append($"}},");
+                }
+                sb.Append(");");
+            } else {
+                foreach (var concat in Concatenations) {
+                    sb.Append(concat.ToString());
+                }
+            }
+            return sb.ToString();
+        }
     }
 
-    // concatenation = factor ;
+    // concatenation = { factor } ;
     public record SbnfConcatenation {
         public List<SbnfFactor> Factors = new();
+        public override string ToString() {
+            StringBuilder sb = new();
+            foreach (var factor in Factors) {
+                sb.Append(factor.ToString());
+            }
+            return sb.ToString();
+        }
     }
 
     // factor = term '?' | term '*' | term '+' | term ;
     public record SbnfFactor {
         public SbnfTerm Term;
         public string? Operator;
+        public override string ToString() {
+            StringBuilder sb = new();
+            sb.Append(Term.ToString());
+            return sb.ToString();
+        }
     }
 
     // term = '(' alternation ')' 
@@ -40,6 +93,42 @@ public class SbnfParser_Parseus : BaseParser {
         public string Type;
         public string Value;
         public SbnfAlternation Alternation;
+        public override string ToString() {
+            StringBuilder sb = new();
+            switch (Type) {
+                case "identifier":
+                    //node
+                    var tmp1 = $"{Value.ToUpper()}_{Guid.NewGuid().ToString().Replace('-', '_')}";
+                    sb.Append($"Node(c, Generated_{Value.ToUpper()}Parser, out self.{tmp1});");
+                    typeMap.Last().value.Add(tmp1);
+                    break;
+                case "string":
+                    //literal
+                    var tmp2 = $"{Value.Substring(1,Value.Length-2).ToUpper()}_{Guid.NewGuid().ToString().Replace('-', '_')}";
+                    sb.Append($"Literal(c, \"{Value.Substring(1,Value.Length-2).ToUpper()}\", out self.{tmp2});");
+                    typeMap.Last().value.Add(tmp2);
+                    break;
+                case "group":
+                    // alt
+                    sb.Append($"Alt(c, c => {{");
+                    sb.Append(Alternation.ToString());
+                    sb.Append($"}});");
+                    break;
+                case "optional":
+                    // opt
+                    sb.Append($"Opt(c, c => {{");
+                    sb.Append(Alternation.ToString());
+                    sb.Append($"}});");
+                    break;
+                case "repeat":
+                    // repeat
+                    sb.Append($"Repeat(c, c => {{");
+                    sb.Append(Alternation.ToString());
+                    sb.Append($"}});");
+                    break;
+            }
+            return sb.ToString();
+        }
     }
     // SbnfDocument = { rule } ;
     private static Parser<SbnfDocument> ParseSbnfDocument = new((c, self) => {
@@ -158,7 +247,47 @@ public class SbnfParser_Parseus : BaseParser {
         var parser = new SbnfParser_Parseus();
         var result = parser.Parse(
             """
-            test := [ 'var' | 'let' ] 'identifier' [ ':' type ] '=' expression ;
+            program         := { statement } ;
+            statement       := variable_declaration
+                            | constant_declaration
+                            | operation
+                            | function_definition
+                            | function_call
+                            | jump_statement
+                            | conditional_statement
+                            | loop_statement
+                            | struct_definition
+                            | package_definition
+                            | include_statement
+                            | destruct_statement
+                            | error_statement ;
+            variable_declaration := "let" identifier value { value } ;
+            constant_declaration := "cst" identifier value { value } ;
+            value           := identifier
+                            | number
+                            | string
+                            | character
+                            | array_definition ;
+            array_definition := "LBRACKET" value { "COMMA" value } "RBRACKET" ;
+            operation       := identifier identifier operator identifier ;
+            operator        := "PLUS" | "MINUS" | "STAR" | "SLASH" ;
+            function_definition := "fnc" identifier [ parameter_list ] block "ret" [ return_value { return_value } ] ;
+            parameter_list  := identifier { identifier } ;
+            return_value    := identifier | value ;
+            function_call   := "cll" identifier { identifier | value } ;
+            jump_statement  := "jmp" identifier | "COLON" identifier ;
+            conditional_statement := "iff" condition block [ "elf" condition block ] [ "els" block ] "ext" ;
+            condition       := identifier logical_operator identifier ;
+            logical_operator := "EQL" | "LES" | "GRT" | "TRU" | "FLS" ;
+            loop_statement  := "whl" condition block "ext"
+                            | "for" identifier value condition value value block "ext"
+                            | "for" identifier identifier block "ext" ;
+            struct_definition := "tbl" identifier [ parameter_list ] block "ext" ;
+            package_definition := "pck" identifier block "ext" ;
+            include_statement := "inc" identifier [ identifier ] ;
+            destruct_statement := "TILDE" identifier ;
+            error_statement := "err" number [ string ] ;
+            block           := { statement } ;
             """);
         // print result as ymal using yamldotnet
         Console.WriteLine(new YamlDotNet.Serialization.SerializerBuilder()
@@ -166,6 +295,6 @@ public class SbnfParser_Parseus : BaseParser {
             .IncludeNonPublicProperties()
             .EnsureRoundtrip()
             .Build().Serialize(result));
+        File.AppendAllText("out.cs", result.ToString().Trim());
     }
-    
 }
